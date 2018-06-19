59d8c9e6a3a3b015ab27ee2b1380e176700ee16b
DK\mdo <mdo@sitecore.net>
2018-06-19 12:39:34 -0400

    Add a new processor to replace UserAgentsDictionary

---
+++ src/Sitecore.Support.225392/Analytics/DataAccess/Dictionaries/UserAgentsDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sitecore.Analytics.DataAccess.Diagnostics.PerformanceCounters;
+using Sitecore.Analytics.Model;
+using Sitecore.Framework.Conditions;
+using Sitecore.StringExtensions;
+using Sitecore.Analytics.DataAccess;
+using Sitecore.Analytics;
+using Sitecore.Analytics.DataAccess.Dictionaries;
+using Sitecore.Caching.Generics;
+
+namespace Sitecore.Support.Analytics.DataAccess.Dictionaries
+{
+  public class UserAgentsDictionary : Sitecore.Analytics.DataAccess.Dictionaries.UserAgentsDictionary
+  {
+    public UserAgentsDictionary(DictionaryBase dictionary) : base(dictionary)
+    {
+      Cache.MaxSize = Sitecore.Configuration.Settings.GetIntSetting("UserAgentDictionaryCacheSize", 100000000);
+    }
+
+    public override TimeSpan CacheExpirationTimeout => TimeSpan.FromSeconds(Sitecore.Configuration.Settings.GetDoubleSetting("UserAgentDictionaryCacheExpiration", 1800));
+  }
+}
---
+++ src/Sitecore.Support.225392/Analytics/DataAccess/Pipelines/InitializeKnownDataDictionaries/InitializeKnownDataDictionariesProcessor.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Analytics.DataAccess.Dictionaries;
+using Sitecore.Analytics.DataAccess.Pipelines.GetDictionaryDataStorage;
+using Sitecore.Framework.Conditions;
+using Sitecore.Analytics.DataAccess.Pipelines.InitializeKnownDataDictionaries;
+
+namespace Sitecore.Support.Analytics.DataAccess.Pipelines.InitializeKnownDataDictionaries
+{
+  public class InitializeKnownDataDictionariesProcessor : InitializeKnownDataDictionariesProcessorBase
+  {
+    public override void Process([NotNull] InitializeKnownDataDictionariesArgs args)
+    {
+      Condition.Requires(args, nameof(args)).IsNotNull();
+      var dictionaryDataArgs = new GetDictionaryDataPipelineArgs();
+      GetDictionaryDataPipeline.Run(dictionaryDataArgs);
+      Condition.Ensures(dictionaryDataArgs.Result).IsNotNull("Check configuration, 'getDictionaryDataStorage' pipeline  must set args.Result property with instance of DictionaryBase type.");
+      args.UserAgentsDictionary = new Sitecore.Support.Analytics.DataAccess.Dictionaries.UserAgentsDictionary(dictionaryDataArgs.Result);
+    }
+  }
+}
