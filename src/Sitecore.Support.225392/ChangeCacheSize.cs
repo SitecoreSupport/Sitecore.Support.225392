5334d2634a0e38578e58c6dac8970eff1184989b
DK\ELB <elb@sitecore.net>
2018-11-15 16:57:45 +0200

    Commits for v8.2.7

--- src/Sitecore.Support.225392/ChangeCacheSize.cs
+++ src/Sitecore.Support.225392/ChangeCacheSize.cs
@@ -1,37 +1,18 @@
 ﻿namespace Sitecore.Support.Analytics.Pipelines.StartAnalytics
 {
-
-  using Sitecore.Diagnostics;
-  using Sitecore.Pipelines;
   using Sitecore.Analytics;
-  using Sitecore.Analytics.Lookups;
-  using Sitecore.Analytics.DataAccess;
   using Sitecore.Analytics.Data.Dictionaries;
   using Sitecore.Analytics.Data.Dictionaries.DictionaryData;
+  using Sitecore.Analytics.DataAccess;
+  using Sitecore.Caching.Generics;
   using Sitecore.Configuration;
-  using System.Reflection;
+  using Sitecore.Diagnostics;
+  using Sitecore.Pipelines;
   using System;
+  using System.Reflection;
+
   public class ChangeCacheSize
   {
-    public virtual void Process(PipelineArgs args)
-    {
-      Assert.ArgumentNotNull(args, "args");
-      LocationsDictionary locationDic = Tracker.Dictionaries.Locations;
-      GeoIpDataDictionary geoIpData = Tracker.Dictionaries.GeoIPs;
-      DeviceDictionary deviceDictionary = Tracker.Dictionaries.Devices;
-
-      if (locationDic == null)
-        Log.Info("Support: location dictionary doesn't exist!", this);
-      if (geoIpData == null)
-        Log.Info("Support: geoIpData dictionary doesn't exist!", this);
-      if (deviceDictionary == null)
-        Log.Info("Support: device dictionary doesn't exist!", this);
-      else
-      {
-        ResetCache(locationDic,geoIpData,deviceDictionary);//reset cache based on config settings node
-      }
-    }
-
     public static DictionaryBase GetDictionaryData()
     {
       GetDictionaryDataPipelineArgs getDictionaryDataPipelineArgs = new GetDictionaryDataPipelineArgs();
@@ -39,25 +20,41 @@
       return Assert.ResultNotNull<DictionaryBase>(getDictionaryDataPipelineArgs.Result, "Check configuration, 'getDictionaryDataStorage' pipeline  must set args.Result property with instance of DictionaryBase type.");
     }
 
-    private void ResetCache(LocationsDictionary locDic,GeoIpDataDictionary geoDic, DeviceDictionary devDic)
+    public virtual void Process(PipelineArgs args)
     {
-      PropertyInfo locCacheProperty = locDic.GetType().BaseType.GetProperty("Cache", BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
-      PropertyInfo geoCacheProperty = geoDic.GetType().BaseType.GetProperty("Cache", BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
-      PropertyInfo devCacheProperty = devDic.GetType().BaseType.GetProperty("Cache", BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
-      Caching.Cache locCache = (Caching.Cache)locCacheProperty.GetValue(locDic);
-      Caching.Cache geoCache = (Caching.Cache)geoCacheProperty.GetValue(geoDic);
-      Caching.Cache devCache = (Caching.Cache)devCacheProperty.GetValue(devDic);
-      if(locCache.MaxSize<=10000000)
-         locCache.MaxSize = Settings.GetIntSetting("LocationsDictionaryCacheSize", 10000000);
-      if (geoCache.MaxSize <= 10000000)
-        geoCache.MaxSize = Settings.GetIntSetting("GeoIpDataDictionaryCacheSize", 10000000);
-      if (devCache.MaxSize <= 10000000)
-        devCache.MaxSize = Settings.GetIntSetting("DeviceDictionaryCacheSize", 10000000);
-        //Sitecore.Caching.Cache para = new Caching.Cache("LocationsDictionaryCache", (long)maxCache);
-        // cacheProperty.SetValue(dictionary, para);
+      Assert.ArgumentNotNull(args, "args");
+
+      UserAgentsDictionary userAgentsDic = Tracker.Dictionaries.BrowserUserAgents;
+      DeviceDictionary deviceDictionary = Tracker.Dictionaries.Devices;
+
+      if (userAgentsDic == null)
+      {
+        Log.Info("Support: BrowserUserAgents dictionary doesn't exist!", this);
+      }
+      if (deviceDictionary == null)
+      {
+        Log.Info("Support: device dictionary doesn't exist!", this);
+      }
+      else
+      {
+        this.ResetCache(userAgentsDic, deviceDictionary);
+      }
+    }
+
+    private void ResetCache(UserAgentsDictionary userAgDic, DeviceDictionary devDic)
+    {
+      PropertyInfo userAgCacheProperty = userAgDic.GetType().BaseType.GetProperty("Cache", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+      PropertyInfo devCacheProperty = devDic.GetType().BaseType.GetProperty("Cache", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+      Cache<Guid> userAgCache = (Cache<Guid>)userAgCacheProperty.GetValue(userAgDic);
+      Cache<Guid> devCache = (Cache<Guid>)devCacheProperty.GetValue(devDic);
+
+      userAgCache.MaxSize = Settings.GetIntSetting("UserAgentDictionaryCacheSize", 0xf4240);
+      devCache.MaxSize = Settings.GetIntSetting("DeviceDictionaryCacheSize", 0xf4240);
+
+      Log.Info($"Size of UserAgentDictionaryCache was changed. UserAgentDictionary.Cache.MaxSize={userAgCache.MaxSize}", this);
+      Log.Info($"Size of DeviceDictionaryCache was changed.  DeviceDictionary.Cache.MaxSize = {devCache.MaxSize} ", this);
       Log.Info("Invoke finished! ", this);
-      
-      
     }
   }
 }
